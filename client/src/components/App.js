a5d5de45d49e5b716b10116ff152f0f9fb06c7cd
Dziugas Butkus <dziubutkus@gmail.com>
2019-02-06 17:25:15 -0700

    add about to navbar #49

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,7 @@ export default class App extends Component {
     super(props);
     this.pages = [
       { title: 't19 We Them Boys', page: ''},
+      { title: 'About', page: 'about'},
       { title: 'Calculator', page: 'calc'},
       { title: 'Options', page: 'options'},
       { title: '\u2699', page: 'settings' }
