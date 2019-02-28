230478d59898e82683cc74ea5114a61ec3894e49
Dziugas Butkus <dziubutkus@gmail.com>
2019-02-27 17:28:55 -0700

    downloads file with calculated distance

--- client/src/components/Application/Itinerary/Itinerary.js
+++ client/src/components/Application/Itinerary/Itinerary.js
@@ -189,9 +189,28 @@ export default class Itinerary extends Component{
                         places: response.body.places,
                         distances: response.body.distances
                     });
+                    this.saveFile();
                 }
             });
     }
+
+    saveFile() {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        var fileName = this.state.filename;
+        var data = this.state;
+        delete(data["filename"]);
+
+        var json = JSON.stringify(data),
+            blob = new Blob([json], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+
     updateLocationOnChange(stateVar, field, value) {
         let location = Object.assign({}, this.state[stateVar]);
         location[field] = value;
