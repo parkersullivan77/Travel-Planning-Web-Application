5368ae3f9925be0e1ada6fc79632feac0c88546f
Kareem <kareemy9000@gmail.com>
2019-03-27 23:41:25 -0600

    Tests for TIP Find

--- server/src/test/java/com/tripco/t19/TIP/TestTIPFind.java
+++ server/src/test/java/com/tripco/t19/TIP/TestTIPFind.java
@@ -1,4 +1,3 @@
-/*
 package com.tripco.t19.TIP;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +8,13 @@ import static org.junit.Assert.assertEquals;
 
 //Test TipFind
 public class TestTIPFind {
-    ArrayList<Map> myPlaces;
+    ArrayList<Map> myPlaces = new ArrayList();
 
     @Before
     public void createValuesForTestCases() {
-        Map<String,String> RandomMap = new <String,String>HashMap();
+       /* Map<String,String> RandomMap = new <String,String>HashMap();
         Map<String,String> Randomtest = new HashMap();
+
         RandomMap.put("id","testID");
         RandomMap.put("name","Fort Collins");
         RandomMap.put("latitude","40.6");
@@ -27,21 +27,24 @@ public class TestTIPFind {
         Randomtest.put("latitude","92.6");
         Randomtest.put("longitude","-32.1");
         Randomtest.put("altitude","4096");
-        Randomtest.put("municipality","Not Larimer");
+        Randomtest.put("municipality","Not Larimer");*/
 
-        myPlaces.add(RandomMap);
+      /*  myPlaces.add(RandomMap);*/
         //myPlaces.add(Randomtest);
     }
     @Test
-    public void testExistingString() {    //test with same origin and destination, should always return 0
-        TIPFind find = new TIPFind("Fort",0,myPlaces,4);
-        find.buildQuery();
-        find.buildResponse();
+    public void testExistingString() {
+        TIPFind find = new TIPFind("Fort Collins Downtown Airport",22,myPlaces,4);
+        find.buildResponse();        //String expect = "random ";
+        String actual = find.toString();
+        String expect = "{match: Fort Collins Downtown Airport, limit: 22, found: 5, places:[{altitude=4939, latitude=40.5882987976, name=Fort Collins Downtown Airport, municipality=null, id=K3V5, longitude=-105.041999817}]";
+        assertEquals("Some test", expect, actual);
     }
+
+
     @Test
     public void testNonExistingString() {
         TIPFind find = new TIPFind("notpartoffile",0,myPlaces,0);
-        find.buildQuery();
         find.buildResponse();
     }
-}*/
+}
