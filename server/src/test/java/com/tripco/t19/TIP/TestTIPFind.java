94e30b6bd7e2e8a2af0027387c7a4903f66be628
Kareem <kareemy9000@gmail.com>
2019-03-28 18:05:18 -0600

    TIPFind + Itinerary.test.js

--- server/src/test/java/com/tripco/t19/TIP/TestTIPFind.java
+++ server/src/test/java/com/tripco/t19/TIP/TestTIPFind.java
@@ -35,6 +35,9 @@ public class TestTIPFind {
     }
 
 /*
+
+*/
+
     @Test
     public void testExistingString() {
         TIPFind find = new TIPFind("Fort Collins Downtown Airport", 22, myPlaces, 0);
@@ -43,14 +46,20 @@ public class TestTIPFind {
         String expect = "{match: Fort Collins Downtown Airport, limit: 22, found: 1, places:[{altitude=4939, latitude=40.5882987976, name=Fort Collins Downtown Airport, municipality=null, id=K3V5, longitude=-105.041999817}]";
         assertEquals("Some test", expect, actual);
     }
-*/
-
     @Test
     public void testStringNotInPlaces() {  //Test for string not in places
         TIPFind find = new TIPFind("subscribe 2 pewdiepie", 22, myPlaces, 0);
         find.buildResponse();
         String actual = find.toString();
         String expect = "{match: subscribe 2 pewdiepie, limit: 22, found: 0, places:[]";
+        assertEquals("Some test", expect, actual);
+    }
+    @Test
+    public void testAnotherStringNotInPlaces() {  //Test for string not in places
+        TIPFind find = new TIPFind("nonexistent string", 22, myPlaces, 0);
+        find.buildResponse();
+        String actual = find.toString();
+        String expect = "{match: nonexistent string, limit: 22, found: 0, places:[]";
         assertEquals("Some test", expect, actual);
     }
 }
