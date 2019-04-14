2c2e48bbdb9065e5fb21cd01d88b20ca3ef98e30
Dziugas Butkus <dziubutkus@gmail.com>
2019-04-13 22:28:16 -0600

    implement JsonArray and JsonObject to store filters

--- server/src/main/java/com/tripco/t19/TIP/TIPConfig.java
+++ server/src/main/java/com/tripco/t19/TIP/TIPConfig.java
@@ -1,5 +1,7 @@
 package com.tripco.t19.TIP;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +26,7 @@ public class TIPConfig extends TIPHeader {
   private List<String> placeAttributes;
   private List<String> optimizations;
   //private List<List<String>> filters;
-  //private JsonObject filters = new JsonObject();
+  private JsonObject filtersJ = new JsonObject();
   private Map filters;
 
   private final transient Logger log = LoggerFactory.getLogger(TIPConfig.class);
@@ -40,10 +42,17 @@ public class TIPConfig extends TIPHeader {
     this.serverName = "T19 We Them Boys";
     this.placeAttributes = Arrays.asList("name", "latitude", "longitude", "id", "municipality", "region", "country", "continent", "altitude");
     this.optimizations = Arrays.asList("none");
-    Map<String, List<String>> temp = new HashMap<>();
+    Map<String, List> temp = new HashMap<>();
     List<String> tempListOfTypes = Arrays.asList("type");
+    JsonArray je = new JsonArray();
+    je.add("airport");
+    je.add("heliport");
+    je.add("balloonport");
+    je.add("closed");
     List<String> tempListOfValues = Arrays.asList("airport", "heliport", "balloonport", "closed");
-    temp.put("types", tempListOfTypes);
+    //filtersJ.add();
+    filtersJ.addProperty("name", "type");
+    filtersJ.add("values", je);
     temp.put("values", tempListOfValues);
     this.filters = temp;
     //temp.put("values", Arrays.asList("airport"));
@@ -53,7 +62,7 @@ public class TIPConfig extends TIPHeader {
   @Override
   public String toString()
   {
-    return "ServerName : " + serverName + " " + "Place Attributes : " + placeAttributes + " " + "Filters : " + filters;
+    return "ServerName : " + serverName + " " + "Place Attributes : " + placeAttributes + " " + "Filters : " + filters + " FiltersJ : " + filtersJ;
    // return "{\"serverName\":"+ "\"" + serverName + "\"," + placeAttributes;
 
     //no need to return log
