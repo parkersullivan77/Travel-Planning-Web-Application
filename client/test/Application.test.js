d9d3c08fc02e4f32674a490300653a2a15b243f5
wstnmssr <wstnmssr@gmail.com>
2019-04-29 19:26:05 -0600

    fix app test

--- client/test/Application.test.js
+++ client/test/Application.test.js
@@ -15,6 +15,7 @@ function testInitialState() {
 
   let actualOptions = app.state().planOptions;
   let expectedOptions = {
+    optimization: {"none": "none", "short": "short"},
     units: {kilometers: 6371, miles: 3959, "nautical miles": 3440},
     activeUnit: 'miles'
   };
